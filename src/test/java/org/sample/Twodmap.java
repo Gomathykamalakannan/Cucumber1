34d6a7624271bb9fec8bca015e0b6cf7deedb442
neethu@24 <nivethaasrini@gmail.com>
2019-05-17 12:10:24 +0530

    Add twodimentional map

---
+++ src/test/java/org/sample/Twodmap.java
@@ -0,0 +1,58 @@
+package org.sample;
+
+import java.util.List;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.cucumber.datatable.DataTable;
+import junit.framework.Assert;
+
+public class Twodmap {
+	public static WebDriver driver;
+	
+	@Given("The user is in webpage")
+	public void the_user_is_in_webpage() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Greens-11\\Desktop\\Manivannan\\selinium\\RSM\\Cucumber\\Driver\\chromedriver.exe");
+		   driver=new ChromeDriver();
+		   driver.get("http://www.demo.guru99.com/telecom/");
+
+	    
+	}
+
+	@When("Fill all the fields")
+	public void fill_all_the_fields(DataTable dataTarif) {
+		driver.findElement(By.xpath("(//a[@href='addtariffplans.php'])[1]")).click();
+		
+		List<Map<String,String>> Details = dataTarif.asMaps(String.class, String.class);
+		
+		driver.findElement(By.id("rental1")).sendKeys(Details.get(1).get("Monthly Rental"));
+		driver.findElement(By.id("local_minutes")).sendKeys(Details.get(0).get("Free local minutes"));
+		driver.findElement(By.id("inter_minutes")).sendKeys(Details.get(1).get("Free internation minutes"));
+		driver.findElement(By.id("sms_pack")).sendKeys(Details.get(1).get("Free sms pack"));
+		driver.findElement(By.id("minutes_charges")).sendKeys(Details.get(0).get("Local per minutes charges"));
+		driver.findElement(By.id("inter_charges")).sendKeys(Details.get(3).get("International per minutes charges"));
+		driver.findElement(By.id("sms_charges")).sendKeys(Details.get(1).get("Sms per charges"));
+	    
+	}
+
+	@When("Click submit")
+	public void click_submit() {
+		driver.findElement(By.xpath("//input[@type=\"submit\"]")).click();	    
+	}
+
+	@SuppressWarnings("deprecation")
+	@Then("Check message")
+	public void check_message() {
+		Assert.assertTrue(driver.findElement(By.xpath("//h2[@style='text-align: center;font-weight: 700;font-size:28px;']")).isDisplayed());
+	   
+	}
+
+
+
+}
---
+++ src/test/resources/Addd/addtwodmap.feature
@@ -0,0 +1,18 @@
+
+
+Feature: Add tarifff
+  I want to use this template for my feature file
+
+
+  Scenario: Title of your scenario
+    Given The user is in webpage
+    When Fill all the fields
+    |Monthly Rental|Free local minutes|Free internation minutes|Free sms pack|Local per minutes charges|International per minutes charges|Sms per charges|
+    |100|200|300|400|500|600|700|
+    |700|600|500|100|300|200|100|
+    |200|400|600|800|100|200|300|
+    |100|200|400|300|400|500|700|
+     When Click submit
+    Then Check message
+
+ 
